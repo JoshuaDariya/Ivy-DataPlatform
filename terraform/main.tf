af87dceaea149561d5c0e88a5170e74d8b02dd54
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-08-29 13:35:08 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,17 @@ terraform {
     }
   }
 }
+
+provider "snowflake" {
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name = "LANDING" #Creating Landing Zone db
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "ivy_wh_tf" #Change to desired name
+  warehouse_size = "small" #Change to desired size
+  auto_suspend   = 60
+}
