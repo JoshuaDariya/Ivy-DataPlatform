5093deea6c9da76cdda70d435f8ff2c790aecde4
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-08-31 18:27:49 +0000

    Adding roles and description about it

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,22 +33,27 @@ resource "snowflake_database" "db" {
 // ------------- ROLES -----------------
 resource "snowflake_role" "loader" {
   name = "LOADER"
+  comment = "For Fivetran connection, dbt job to create zero copy clones"
 }
 
 resource "snowflake_role" "transformer_dev" {
   name = "TRANSFORMER_DEV"
+  comment = "For developers and Dev dbt connection"
 }
 
 resource "snowflake_role" "transformer_stage" {
   name = "TRANSFORMER_STAGE"
+  comment = "For Stage dbt connection"
 }
 
 resource "snowflake_role" "transformer_prod" {
   name = "TRANSFORMER_PROD"
+  comment = "For Prod dbt job connection"
 }
 
 resource "snowflake_role" "reporter" {
   name = "REPORTER"
+  comment = "For BI analysts and PowerBI connection"
 }
 
 // ------------- WAREHOUSE -----------------
