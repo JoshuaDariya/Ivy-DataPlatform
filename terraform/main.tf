b6500f32231e62b0f780059046c72ef6ccbd333c
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-18 19:54:23 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,13 +106,34 @@ resource "snowflake_database_grant" "prod_access_grant" {
 }
 // ------------- COST ALERTS -----------------
 resource "snowflake_resource_monitor" "monitor_1" {
-  name         = "Cost Monitoring"
+  name         = "Fivetran"
   credit_quota = 100
 
   end_timestamp   = "2024-09-01 00:00"
 
+  warehouses   = ["PC_FIVETRAN_WH"]
+
 }
 
+resource "snowflake_resource_monitor" "monitor_2" {
+  name         = "POWER BI"
+  credit_quota = 100
+
+  end_timestamp   = "2024-09-01 00:00"
+
+  warehouses   = ["POWER_BI_WH"]
+
+}
+
+resource "snowflake_resource_monitor" "monitor_3" {
+  name         = "DEV USERS"
+  credit_quota = 100
+
+  end_timestamp   = "2024-09-01 00:00"
+
+  warehouses   = ["IVY_WH"]
+
+}
 
 // ------------- WAREHOUSE -----------------
 resource "snowflake_warehouse" "warehouse" {
