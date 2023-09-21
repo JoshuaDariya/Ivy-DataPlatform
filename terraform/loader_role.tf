58185adce9d94a2f6d4ca3f61bf4495823a9539c
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-21 14:27:47 +0000

    Testing access for loader 1

--- terraform/loader_role.tf
+++ terraform/loader_role.tf
@@ -2,21 +2,20 @@
 // ------------- LOADER ROLE ACCESS -----------------
 
 resource "snowflake_grant_privileges_to_role" "fivetran_access_grant" {
-  privileges = ["CREATE SCHEMA", "MODIFY", "MONITOR"]
-  role_name  = "LOADER"
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = var.landing
-  }
-}
-
-resource "snowflake_grant_privileges_to_role" "fivetran_access_grant_all_schemas" {
-  privileges = ["USAGE"]
+  privileges = ["USAGE","CREATE SCHEMA", "MODIFY", "MONITOR"]
   role_name  = "LOADER"
   on_schema {
     all_schemas_in_database = var.landing
   }
 }
+
+# resource "snowflake_grant_privileges_to_role" "fivetran_access_grant_all_schemas" {
+#   privileges = ["USAGE"]
+#   role_name  = "LOADER"
+#   on_schema {
+#     all_schemas_in_database = var.landing
+#   }
+# }
 
 resource "snowflake_grant_privileges_to_role" "fivetran_future_access_grant" {
   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
