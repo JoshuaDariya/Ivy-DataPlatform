c7a9797553b3cefc1ec1265de0d16d67527e725a
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-21 16:21:37 +0000

    Visual appeal change

--- terraform/loader_role.tf
+++ terraform/loader_role.tf
@@ -10,20 +10,20 @@ resource "snowflake_grant_privileges_to_role" "fivetran_access_db_grant" {
 }
 
 
-resource "snowflake_grant_privileges_to_role" "fivetran_access_schema_grant" {
-  privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
-  role_name  = "LOADER"
-  on_schema {
-    all_schemas_in_database = var.landing
-  }
-}
-
-
 resource "snowflake_grant_privileges_to_role" "fivetran_future_access_grant" {
   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
   role_name  = "LOADER"
   on_schema {
     future_schemas_in_database = var.landing
+  }
+}
+
+
+resource "snowflake_grant_privileges_to_role" "fivetran_access_schema_grant" {
+  privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
+  role_name  = "LOADER"
+  on_schema {
+    all_schemas_in_database = var.landing
   }
 }
 
