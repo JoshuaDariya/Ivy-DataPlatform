fd4444a09d5c71a71e589e9f34d6cab0d1c3a4d3
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-21 13:58:51 +0000

    Loader added

--- terraform/loader_role.tf
+++ terraform/loader_role.tf
@@ -1,19 +1,19 @@
 
 // ------------- LOADER ROLE ACCESS -----------------
 
-# resource "snowflake_grant_privileges_to_role" "fivetran_access_grant" {
-#   privileges = ["USAGE","CREATE SCHEMA", "MODIFY", "MONITOR"]
-#   role_name  = "LOADER"
-#   on_account_object {
-#     object_type = "DATABASE"
-#     object_name = var.landing
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "fivetran_access_grant" {
+  privileges = ["USAGE","CREATE SCHEMA", "MODIFY", "MONITOR"]
+  role_name  = "LOADER"
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = var.landing
+  }
+}
 
-# resource "snowflake_grant_privileges_to_role" "fivetran_future_access_grant" {
-#   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
-#   role_name  = "LOADER"
-#   on_schema {
-#     future_schemas_in_database = var.landing
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "fivetran_future_access_grant" {
+  privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
+  role_name  = "LOADER"
+  on_schema {
+    future_schemas_in_database = var.landing
+  }
+}
