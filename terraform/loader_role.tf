0f90a1f98e578de1ffdc8c4c23803161e0bb1853
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-21 14:36:28 +0000

    Loader Role testing 1

--- terraform/loader_role.tf
+++ terraform/loader_role.tf
@@ -2,20 +2,13 @@
 // ------------- LOADER ROLE ACCESS -----------------
 
 resource "snowflake_grant_privileges_to_role" "fivetran_access_grant" {
-  privileges = ["USAGE","CREATE SCHEMA", "MODIFY", "MONITOR"]
+  privileges = ["USAGE","CREATE TABLE","MODIFY", "MONITOR"]
   role_name  = "LOADER"
   on_schema {
     all_schemas_in_database = var.landing
   }
 }
 
-# resource "snowflake_grant_privileges_to_role" "fivetran_access_grant_all_schemas" {
-#   privileges = ["USAGE"]
-#   role_name  = "LOADER"
-#   on_schema {
-#     all_schemas_in_database = var.landing
-#   }
-# }
 
 resource "snowflake_grant_privileges_to_role" "fivetran_future_access_grant" {
   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
