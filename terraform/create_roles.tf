8cff2c3a4e2843a1fab7b4fbe4fc5c912a22bdaa
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-22 17:15:10 +0000

    Creation of roles are not needed

--- terraform/create_roles.tf
+++ terraform/create_roles.tf
@@ -1,25 +1,25 @@
 // ------------- ROLES (This should eventually match Azure Provisions) -----------------
-resource "snowflake_role" "loader" {
-  name = var.loader_role
-  comment = "For Fivetran connection"
-}
+# resource "snowflake_role" "loader" {
+#   name = var.loader_role
+#   comment = "For Fivetran connection"
+# }
 
-resource "snowflake_role" "transformer_dev" {
-  name = var.developer_role
-  comment = "For developers and Dev dbt connection"
-}
+# resource "snowflake_role" "transformer_dev" {
+#   name = var.developer_role
+#   comment = "For developers and Dev dbt connection"
+# }
 
-resource "snowflake_role" "transformer_qa" {
-  name = var.qa_role
-  comment = "For QA dbt connection"
-}
+# resource "snowflake_role" "transformer_qa" {
+#   name = var.qa_role
+#   comment = "For QA dbt connection"
+# }
 
-resource "snowflake_role" "transformer_prod" {
-  name = var.prod_role
-  comment = "For Prod dbt job connection"
-}
+# resource "snowflake_role" "transformer_prod" {
+#   name = var.prod_role
+#   comment = "For Prod dbt job connection"
+# }
 
-resource "snowflake_role" "reporter" {
-  name = var.powerbi_role
-  comment = "For BI analysts and PowerBI connection"
-}
+# resource "snowflake_role" "reporter" {
+#   name = var.powerbi_role
+#   comment = "For BI analysts and PowerBI connection"
+# }
