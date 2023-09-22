1b24c6de85ffc7a75d84f7c3ab23219949d7a3a9
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-09-22 18:21:16 +0000

    Privileges added back -reset

--- terraform/loader_role.tf
+++ terraform/loader_role.tf
@@ -1,75 +1,75 @@
 
-# // ------------- LOADER ROLE ACCESS -----------------
-# resource "snowflake_grant_privileges_to_role" "loader_access_db_grant" {
-#   privileges = ["USAGE","MODIFY", "MONITOR","CREATE SCHEMA"]
-#   role_name  = var.loader_role
-#   on_account_object {
-#     object_type = "DATABASE"
-#     object_name = var.landing
-#   }
-# }
+// ------------- LOADER ROLE ACCESS -----------------
+resource "snowflake_grant_privileges_to_role" "loader_access_db_grant" {
+  privileges = ["USAGE","MODIFY", "MONITOR","CREATE SCHEMA"]
+  role_name  = var.loader_role
+  on_account_object {
+    object_type = "DATABASE"
+    object_name = var.landing
+  }
+}
 
 
-# resource "snowflake_grant_privileges_to_role" "loader_future_access_grant" {
-#   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
-#   role_name  = var.loader_role
-#   on_schema {
-#     future_schemas_in_database = var.landing
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "loader_future_access_grant" {
+  privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
+  role_name  = var.loader_role
+  on_schema {
+    future_schemas_in_database = var.landing
+  }
+}
 
 
-# resource "snowflake_grant_privileges_to_role" "loader_access_schema_grant" {
-#   privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
-#   role_name  = var.loader_role
-#   on_schema {
-#     all_schemas_in_database = var.landing
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "loader_access_schema_grant" {
+  privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
+  role_name  = var.loader_role
+  on_schema {
+    all_schemas_in_database = var.landing
+  }
+}
 
-# //--------- LANDING ACCESS TO CURRENT TABLES, VIEWS --------
+//--------- LANDING ACCESS TO CURRENT TABLES, VIEWS --------
 
-# resource "snowflake_grant_privileges_to_role" "loader_access_all_tables_grant" {
-#   privileges = ["SELECT", "INSERT"]
-#   role_name  = var.loader_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "TABLES"
-#         in_database = var.landing
-#     }
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "loader_access_all_tables_grant" {
+  privileges = ["SELECT", "INSERT"]
+  role_name  = var.loader_role
+  on_schema_object {
+    all {
+        object_type_plural = "TABLES"
+        in_database = var.landing
+    }
+  }
+}
 
-# resource "snowflake_grant_privileges_to_role" "loader_access_all_views_grant" {
-#   privileges = ["SELECT", "INSERT"]
-#   role_name  = var.loader_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "VIEWS"
-#         in_database = var.landing
-#     }
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "loader_access_all_views_grant" {
+  privileges = ["SELECT", "INSERT"]
+  role_name  = var.loader_role
+  on_schema_object {
+    all {
+        object_type_plural = "VIEWS"
+        in_database = var.landing
+    }
+  }
+}
 
 
-# // ------------ FUTURE TABLES AND VIEWS -----------------
-# resource "snowflake_grant_privileges_to_role" "loader_access_future_tables_landing" {
-#   privileges = ["SELECT","INSERT"]
-#   role_name  = var.loader_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "TABLES"
-#       in_database        = var.landing
-#     }
-#   }
-# }
-# resource "snowflake_grant_privileges_to_role" "loader_access_future_views_landing" {
-#   privileges = ["SELECT","INSERT"]
-#   role_name  = var.loader_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "VIEWS"
-#       in_database        = var.landing
-#     }
-#   }
-# }
+// ------------ FUTURE TABLES AND VIEWS -----------------
+resource "snowflake_grant_privileges_to_role" "loader_access_future_tables_landing" {
+  privileges = ["SELECT","INSERT"]
+  role_name  = var.loader_role
+  on_schema_object {
+    future {
+      object_type_plural = "TABLES"
+      in_database        = var.landing
+    }
+  }
+}
+resource "snowflake_grant_privileges_to_role" "loader_access_future_views_landing" {
+  privileges = ["SELECT","INSERT"]
+  role_name  = var.loader_role
+  on_schema_object {
+    future {
+      object_type_plural = "VIEWS"
+      in_database        = var.landing
+    }
+  }
+}
--- terraform/transformer_prod_role.tf
+++ terraform/transformer_prod_role.tf
@@ -1,160 +1,160 @@
-#  // ------------- TRANSFORMER PROD ROLE ACCESS -----------------
+ // ------------- TRANSFORMER PROD ROLE ACCESS -----------------
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_db_grant_landing" {
-#   privileges = ["USAGE", "MONITOR"]
-#   role_name  = var.prod_role
-#   on_account_object {
-#     object_type = "DATABASE"
+resource "snowflake_grant_privileges_to_role" "prod_access_db_grant_landing" {
+  privileges = ["USAGE", "MONITOR"]
+  role_name  = var.prod_role
+  on_account_object {
+    object_type = "DATABASE"
 
-#     object_name = var.landing
-#   }
-# }
+    object_name = var.landing
+  }
+}
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_db_grant_prod" {
-#   privileges = ["USAGE", "MODIFY", "MONITOR","CREATE SCHEMA"]
-#   role_name  = var.prod_role
-#   on_account_object {
-#     object_type = "DATABASE"
+resource "snowflake_grant_privileges_to_role" "prod_access_db_grant_prod" {
+  privileges = ["USAGE", "MODIFY", "MONITOR","CREATE SCHEMA"]
+  role_name  = var.prod_role
+  on_account_object {
+    object_type = "DATABASE"
 
-#     object_name = var.prod
-#   }
-# }
-
-
-# // ---------------- FUTURE GRANTS --------------------
-
-# resource "snowflake_grant_privileges_to_role" "prod_future_access_grant_landing" {
-#   privileges = ["USAGE","MONITOR"]
-#   role_name  = var.prod_role
-#   on_schema {
-
-#     future_schemas_in_database = var.landing
-#   }
-# }
-
-# resource "snowflake_grant_privileges_to_role" "prod_future_access_grant_prod" {
-#   privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
-#   role_name  = var.prod_role
-#   on_schema {
-
-#     future_schemas_in_database = var.prod
-#   }
-# }
+    object_name = var.prod
+  }
+}
 
 
-# // ---------------- CURRENT SCHEMA GRANTS --------------------
+// ---------------- FUTURE GRANTS --------------------
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_schema_grant_landing" {
-#   privileges = ["USAGE", "MONITOR"]
-#   role_name  = var.prod_role
-#   on_schema {
-#     all_schemas_in_database = var.landing
-#   }
-# }
-# resource "snowflake_grant_privileges_to_role" "prod_access_schema_grant_prod" {
-#   privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
-#   role_name  = var.prod_role
-#   on_schema {
-#     all_schemas_in_database = var.prod
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "prod_future_access_grant_landing" {
+  privileges = ["USAGE","MONITOR"]
+  role_name  = var.prod_role
+  on_schema {
 
+    future_schemas_in_database = var.landing
+  }
+}
 
-# //--------- LANDING ACCESS TO CURRENT TABLES, VIEWS ------------------
+resource "snowflake_grant_privileges_to_role" "prod_future_access_grant_prod" {
+  privileges = ["MODIFY", "CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE", "USAGE","MONITOR"]
+  role_name  = var.prod_role
+  on_schema {
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_all_tables_grant_landing" {
-#   privileges = ["SELECT"]
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "TABLES"
-
-#         in_database = var.landing
-#     }
-#   }
-# }
-
-# resource "snowflake_grant_privileges_to_role" "prod_access_all_views_grant_landing" {
-#   privileges = ["SELECT"]
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "VIEWS"
-
-#         in_database = var.landing
-#     }
-#   }
-# }
+    future_schemas_in_database = var.prod
+  }
+}
 
 
-# // -------------- PROD ACCESS TO CURRENT TABLES AND VIEWS -------------------
-# resource "snowflake_grant_privileges_to_role" "prod_access_all_tables_grant_prod" {
-#   all_privileges = true
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "TABLES"
+// ---------------- CURRENT SCHEMA GRANTS --------------------
 
-#         in_database = var.prod
-#     }
-#   }
-# }
+resource "snowflake_grant_privileges_to_role" "prod_access_schema_grant_landing" {
+  privileges = ["USAGE", "MONITOR"]
+  role_name  = var.prod_role
+  on_schema {
+    all_schemas_in_database = var.landing
+  }
+}
+resource "snowflake_grant_privileges_to_role" "prod_access_schema_grant_prod" {
+  privileges = ["USAGE","CREATE TABLE", "CREATE VIEW", "CREATE DYNAMIC TABLE","MODIFY", "MONITOR"]
+  role_name  = var.prod_role
+  on_schema {
+    all_schemas_in_database = var.prod
+  }
+}
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_all_views_grant_prod" {
-#   all_privileges = true
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     all {
-#         object_type_plural = "VIEWS"
 
-#         in_database = var.prod
-#     }
-#   }
-# }
+//--------- LANDING ACCESS TO CURRENT TABLES, VIEWS ------------------
 
-# // -------- FUTURE TABLES ----------
+resource "snowflake_grant_privileges_to_role" "prod_access_all_tables_grant_landing" {
+  privileges = ["SELECT"]
+  role_name  = var.prod_role
+  on_schema_object {
+    all {
+        object_type_plural = "TABLES"
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_future_tables_landing" {
-#   privileges = ["SELECT"]
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "TABLES"
-#       in_database        = var.landing
-#     }
-#   }
-# }
-# resource "snowflake_grant_privileges_to_role" "prod_access_future_tables_prod" {
-#   all_privileges = true
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "TABLES"
-#       in_database        = var.prod
-#     }
-#   }
-# }
+        in_database = var.landing
+    }
+  }
+}
 
-# // -------- FUTURE VIEWS ----------
+resource "snowflake_grant_privileges_to_role" "prod_access_all_views_grant_landing" {
+  privileges = ["SELECT"]
+  role_name  = var.prod_role
+  on_schema_object {
+    all {
+        object_type_plural = "VIEWS"
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_future_views_landing" {
-#   privileges = ["SELECT"]
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "VIEWS"
-#       in_database        = var.landing
-#     }
-#   }
-# }
+        in_database = var.landing
+    }
+  }
+}
 
-# resource "snowflake_grant_privileges_to_role" "prod_access_future_views_prod" {
-#   all_privileges = true
-#   role_name  = var.prod_role
-#   on_schema_object {
-#     future {
-#       object_type_plural = "VIEWS"
-#       in_database        = var.prod
-#     }
-#   }
-# }
+
+// -------------- PROD ACCESS TO CURRENT TABLES AND VIEWS -------------------
+resource "snowflake_grant_privileges_to_role" "prod_access_all_tables_grant_prod" {
+  all_privileges = true
+  role_name  = var.prod_role
+  on_schema_object {
+    all {
+        object_type_plural = "TABLES"
+
+        in_database = var.prod
+    }
+  }
+}
+
+resource "snowflake_grant_privileges_to_role" "prod_access_all_views_grant_prod" {
+  all_privileges = true
+  role_name  = var.prod_role
+  on_schema_object {
+    all {
+        object_type_plural = "VIEWS"
+
+        in_database = var.prod
+    }
+  }
+}
+
+// -------- FUTURE TABLES ----------
+
+resource "snowflake_grant_privileges_to_role" "prod_access_future_tables_landing" {
+  privileges = ["SELECT"]
+  role_name  = var.prod_role
+  on_schema_object {
+    future {
+      object_type_plural = "TABLES"
+      in_database        = var.landing
+    }
+  }
+}
+resource "snowflake_grant_privileges_to_role" "prod_access_future_tables_prod" {
+  all_privileges = true
+  role_name  = var.prod_role
+  on_schema_object {
+    future {
+      object_type_plural = "TABLES"
+      in_database        = var.prod
+    }
+  }
+}
+
+// -------- FUTURE VIEWS ----------
+
+resource "snowflake_grant_privileges_to_role" "prod_access_future_views_landing" {
+  privileges = ["SELECT"]
+  role_name  = var.prod_role
+  on_schema_object {
+    future {
+      object_type_plural = "VIEWS"
+      in_database        = var.landing
+    }
+  }
+}
+
+resource "snowflake_grant_privileges_to_role" "prod_access_future_views_prod" {
+  all_privileges = true
+  role_name  = var.prod_role
+  on_schema_object {
+    future {
+      object_type_plural = "VIEWS"
+      in_database        = var.prod
+    }
+  }
+}
