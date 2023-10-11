7b784b276d8170edda53eb0c89bc09c99250253f
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-10-11 20:01:22 +0000

    Added table_creation.tf

---
+++ terraform/table_creation.tf
@@ -0,0 +1,112 @@
+resource "snowflake_table" "ingest_ivyfotoexport" {
+  database            = var.landing
+  schema              = "RAINTREE"
+  name                = "IVYFOTOEXPORT"
+  change_tracking     = true
+
+
+  column {
+    name = "score"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "goal"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "latest"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "tab"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "assessment_section"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "patient"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "caseindex"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "fotoindex"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "episodeid"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "reportid"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "surveystarted"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "surveycompleted"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "reporttype"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "mcii"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "mdc"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "episodeduration"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "physicalfschange"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "riskadjfoto"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "visitsperepisode"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "dischargefsscore"
+    type = "VARCHAR"
+  }
+
+  column {
+    name = "avgsatisfaction"
+    type = "VARCHAR"
+  }
+}
