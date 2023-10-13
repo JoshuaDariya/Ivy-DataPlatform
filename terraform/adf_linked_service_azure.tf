94ad0455833b52bb16a25a275e5863eb204b8d03
Ling He <ling.he@ivyrehab.com>
2023-10-13 13:49:31 +0000

    Test

--- terraform/adf_linked_service_azure.tf
+++ terraform/adf_linked_service_azure.tf
@@ -17,7 +17,7 @@ resource "azurerm_data_factory_linked_service_key_vault" "sastoken" {
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "linkedservice_azureblobstorage" {
   name            = "linkedservice_azureblobstorage"
   data_factory_id = azurerm_data_factory.adf.id
-
+  
   sas_uri = "https://ivydwstoragedev.blob.core.windows.net/analytics/"
   key_vault_sas_token {
     linked_service_name = azurerm_data_factory_linked_service_key_vault.sastoken.name
