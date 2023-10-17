9c4225142ecaaa943888127d06c718961ac9f594
Ling He <ling.he@ivyrehab.com>
2023-10-17 22:40:07 +0000

    updated factory name

--- terraform/adf_creation.tf
+++ terraform/adf_creation.tf
@@ -1,4 +1,14 @@
-resource "azurerm_data_factory" "adf_dev" {
+# resource "azurerm_data_factory" "adf_dev" {
+#   name                = "Ivy-adf-main"
+#   location            = data.azurerm_resource_group.imported_rg_dev.location
+#   resource_group_name = data.azurerm_resource_group.imported_rg_dev.name
+
+#     identity {
+#     type = "SystemAssigned"
+#   }  
+# }
+
+resource "azurerm_data_factory" "adf_main" {
   name                = "Ivy-adf-main"
   location            = data.azurerm_resource_group.imported_rg_dev.location
   resource_group_name = data.azurerm_resource_group.imported_rg_dev.name
