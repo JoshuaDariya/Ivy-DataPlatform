3030230f30ee3a20e88545f9eae04a7e4792db9a
Ling He <ling.he@ivyrehab.com>
2023-10-17 22:16:15 +0000

    test

--- terraform/adf_creation.tf
+++ terraform/adf_creation.tf
@@ -8,7 +8,7 @@ resource "azurerm_data_factory" "adf_dev" {
   }  
 }
 
-resource "azurerm_data_factory" "adf_dev" {
+resource "azurerm_data_factory" "adf_main" {
   name                = "Ivy-dataplatform-snowflake"
   location            = data.azurerm_resource_group.imported_rg_dev.location
   resource_group_name = data.azurerm_resource_group.imported_rg_dev.name
