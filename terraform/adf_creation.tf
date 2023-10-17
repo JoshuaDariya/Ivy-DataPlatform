99c088dea3200296d1a4f962b2a1013e23c5b21a
Ling He <ling.he@ivyrehab.com>
2023-10-17 22:24:33 +0000

    test2

--- terraform/adf_creation.tf
+++ terraform/adf_creation.tf
@@ -1,15 +1,5 @@
 resource "azurerm_data_factory" "adf_dev" {
-  name                = "Ivy-dataplatform-snowflake-test"
-  location            = data.azurerm_resource_group.imported_rg_dev.location
-  resource_group_name = data.azurerm_resource_group.imported_rg_dev.name
-
-    identity {
-    type = "SystemAssigned"
-  }  
-}
-
-resource "azurerm_data_factory" "adf_main" {
-  name                = "Ivy-dataplatform-snowflake"
+  name                = "Ivy-adf-main"
   location            = data.azurerm_resource_group.imported_rg_dev.location
   resource_group_name = data.azurerm_resource_group.imported_rg_dev.name
 
