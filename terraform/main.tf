2447d7c734080e5e3c80957308483fa69c3c0145
Alex Crosslen <alex.crosslen@ivyrehab.com>
2023-10-19 14:40:33 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.70"
+      version = "0.73"
     }
     azurerm = {
       source = "hashicorp/azurerm"
