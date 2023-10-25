d508238433825081fe274b52fc33106e1fd06b31
Nisaa Pender <nisaa.pender@ivyrehab.com>
2023-10-25 20:59:29 +0000

    ... changing the name yet again

--- terraform/table_creation.tf
+++ terraform/table_creation.tf
@@ -2894,7 +2894,7 @@ database            = var.landing
 resource "snowflake_table" "ingest_ivy_locations_report_export" {
 database            = var.landing
   schema              = "RAINTREE"
-  name                = "IVY_LOCATIONS_REPORT"
+  name                = "IVY_LOCATIONS_REPORT_EXPORT"
   change_tracking     = true
 
   column {
@@ -2996,7 +2996,7 @@ database            = var.landing
 resource "snowflake_table" "ingest_z_ivy_locations_report_export" {
 database            = var.landing
   schema              = "RAINTREE"
-  name                = "Z_IVY_LOCATIONS_REPORT"
+  name                = "Z_IVY_LOCATIONS_REPORT_EXPORT"
   change_tracking     = true
 
   column {
