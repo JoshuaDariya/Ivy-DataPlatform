9e7791af29e6303a07e9b1b09560a75b83f7e7d5
Nisaa Pender <nisaa.pender@ivyrehab.com>
2024-01-16 15:02:48 +0000

    Updated adaptive.tf, added azure tenant id

--- terraform/adaptive.tf
+++ terraform/adaptive.tf
@@ -1,4 +1,3 @@
-//Might need Loader Access (DONE)
 resource "snowflake_file_format" "adaptive_parquet_file_format" {
   name        = "PARQUET_FORMAT"
   database    = var.landing
@@ -7,20 +6,15 @@ resource "snowflake_file_format" "adaptive_parquet_file_format" {
   binary_as_text = true
 }
 
-//Might need Loader Access (DONE)
 resource "snowflake_storage_integration" "azure_adaptive_integration" {
   name    = var.adaptive_azure_int
   type    = "EXTERNAL_STAGE"
-
   enabled = true
-
-  storage_allowed_locations = [var.azure_adaptive_url_storage_allowed_locations]
-
+  storage_allowed_locations = var.azure_adaptive_url_storage_allowed_locations
   storage_provider         = "Azure"
-  storage_aws_role_arn     = var.storage_aws_role_arn ~
+  azure_tenant_id = var.azure_tenant
 }
 
-//Might need Loader Access (DONE)
 resource "snowflake_stage" "raintree_v2_stage" {
   name        = var.adaptive_stage
   url         = var.azure_adaptive_url_storage_allowed_locations
