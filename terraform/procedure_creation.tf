7d0cf2b48281a7037c836658384cc0c39d56631b
Alex Crosslen <alex.crosslen@ivyrehab.com>
2024-01-17 19:01:20 +0000

    Adding insert_ingestion_fail_log

--- terraform/procedure_creation.tf
+++ terraform/procedure_creation.tf
@@ -605,3 +605,64 @@ catch (err) {
 }
 EOF
 }
+
+resource "snowflake_procedure" "insert_ingestion_fail_log" {
+  name     = "INSERT_INGESTION_FAIL_LOG"
+  database = var.landing
+  schema   = "RAINTREE"
+  language = "JAVASCRIPT"
+
+  arguments {
+    name = "BATCH_NUMBER"
+    type = "varchar"
+  }
+  arguments {
+    name = "FAIL_AREA"
+    type = "varchar"
+  }
+    arguments {
+    name = "TABLE_NAME"
+    type = "varchar"
+  }
+
+  arguments {
+    name = "ERROR"
+    type = "varchar"
+  }
+
+  comment             = "Inserts records into the Ingestion Fail Log table."
+  return_type         = "VARCHAR(16777216)"
+  execute_as          = "CALLER"
+  return_behavior     = "IMMUTABLE"
+  statement           = <<EOF
+  try {
+    // Set the schema and table names
+    var schemaName = ''RAINTREE'';
+    var tableName = ''INGESTION_FAIL_LOG'';
+
+    // Check if the table exists
+    var tableExistsQuery = `SELECT COUNT(*) AS TABLE_COUNT FROM information_schema.tables WHERE table_schema = ''${schemaName}'' AND table_name = ''${tableName}''`;
+    var tableExistsResult = snowflake.execute({sqlText: tableExistsQuery});
+    
+    if (tableExistsResult.next() && tableExistsResult.getColumnValue(''TABLE_COUNT'') > 0) {
+        // Table exists, insert data
+        var insertQuery = `INSERT INTO ${tableName} (fail_date, batchNumber, tableName, failArea, error) VALUES (CURRENT_DATE(), :1, :2, :3, :4)`;
+        snowflake.execute({sqlText: insertQuery, binds: [BATCH_NUMBER, TABLE_NAME, FAIL_AREA, ERROR]});
+        
+        return ''Data inserted into '' + tableName + '' successfully.'';
+    } else {
+        // Table does not exist, create it
+        var createTableQuery = `CREATE TABLE ${tableName} (fail_date DATE, batchNumber VARCHAR, tableName VARCHAR, failArea VARCHAR, error VARCHAR)`;
+        snowflake.execute({sqlText: createTableQuery});
+
+        // Insert data into the newly created table
+        var insertQuery = `INSERT INTO ${tableName} (fail_date, batchNumber, tableName, failArea, error) VALUES (CURRENT_DATE(), :1, :2, :3, :4)`;
+        snowflake.execute({sqlText: insertQuery, binds: [BATCH_NUMBER, TABLE_NAME, FAIL_AREA, ERROR]});
+
+        return ''Table '' + tableName + '' created and data inserted successfully.'';
+    }
+} catch (err) {
+    return ''Error: '' + err;
+}
+EOF
+}
