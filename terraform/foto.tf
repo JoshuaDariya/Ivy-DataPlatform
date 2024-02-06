62897860501da9de753f84bf2843bdb23587c6c6
Ling He <ling.he@ivyrehab.com>
2024-02-06 16:27:44 +0000

    test

---
+++ terraform/foto.tf
@@ -0,0 +1,25 @@
+resource "snowflake_file_format" "foto_parquet_file_format" {
+  name        = "FILEFORMAT_PARQUET"
+  database    = var.landing
+  schema      = var.foto_schema
+  format_type = "PARQUET"
+  binary_as_text = true
+}
+
+resource "snowflake_storage_integration" "azure_foto_integration" {
+  name    = var.foto_azure_int
+  type    = "EXTERNAL_STAGE"
+  enabled = true
+  storage_allowed_locations = [var.azure_foto_url_storage_allowed_locations]
+  storage_provider         = "AZURE"
+  azure_tenant_id     = var.azure_tenant
+}
+
+resource "snowflake_stage" "snowflake_foto_stage" {
+  name        = var.foto_stage
+  url         = var.azure_foto_url_storage_allowed_locations
+  storage_integration = var.foto_azure_int
+  database    = var.landing
+  schema      = var.foto_schema 
+
+}
