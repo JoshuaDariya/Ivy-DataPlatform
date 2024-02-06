e84be7ca1103ef647774f1882a5380a8a95af37e
Ling He <ling.he@ivyrehab.com>
2024-02-06 16:05:25 +0000

    test

--- terraform/schema_creation.tf
+++ terraform/schema_creation.tf
@@ -25,11 +25,3 @@ resource "snowflake_schema" "presentation_schema" {
   name       = "PRESENTATION" #Name would want to be changed to something better
   is_managed = false
 }
-
-resource "snowflake_schema" "foto_schema" {
-
-  database = "LANDING"
-  name       = "FOTO" 
-  is_managed = false
-}
-
