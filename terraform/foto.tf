a342478e958100313110b52ce2ffb0d79e0d6469
Ling He <ling.he@ivyrehab.com>
2024-02-06 16:24:16 +0000

    test

--- terraform/foto.tf
+++
@@ -1,8 +0,0 @@
-resource "snowflake_storage_integration" "azure_foto_integration" {
-  name    = var.foto_azure_int
-  type    = "EXTERNAL_STAGE"
-  enabled = true
-  storage_allowed_locations = [var.azure_foto_url_storage_allowed_locations]
-  storage_provider         = "AZURE"
-  azure_tenant_id     = var.azure_tenant
-}
