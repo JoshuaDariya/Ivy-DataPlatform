f1484ba91d9dba602394c08c7236d914485bd447
Ling He <ling.he@ivyrehab.com>
2024-02-06 14:45:35 +0000

    Added foto schema to terraform.

--- terraform/schema_creation.tf
+++ terraform/schema_creation.tf
@@ -26,3 +26,11 @@ resource "snowflake_schema" "presentation_schema" {
   is_managed = false
 }
 
+resource "snowflake_schema" "foto_schema" {
+        for_each = var.databases_with_schema
+
+  database = each.key
+  name       = "FOTO" 
+  is_managed = false
+}
+
