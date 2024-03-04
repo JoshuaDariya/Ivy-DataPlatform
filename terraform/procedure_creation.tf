0cda2123624735c24ea9e2d45153680a2d28b1df
Alex Crosslen <alex.crosslen@ivyrehab.com>
2024-03-04 11:23:56 -0500

    Fixing minor error with the catch

--- terraform/procedure_creation.tf
+++ terraform/procedure_creation.tf
@@ -907,7 +907,8 @@ resource "snowflake_procedure" "infer_schema_and_copy_data" {
         
     return tableCreatedCounter;    
   } catch (err) {
-    var callFailLogSQL = `CALL INSERT_INGESTION_FAIL_LOG('$${BATCH_ID}','Failure when running infer_schema_and_copy_data', '$${INCREMENT_TABLE_NAME}','$${err}')`;
+    var errWithoutQuotes = err.replace(/'/g, '');
+    var callFailLogSQL = `CALL INSERT_INGESTION_FAIL_LOG('$${BATCH_ID}','Failure when running infer_schema_and_copy_data', '$${INCREMENT_TABLE_NAME}','$${errWithoutQuotes}')`;
     var logFail = snowflake.execute({ sqlText: callFailLogSQL });
 }
 
