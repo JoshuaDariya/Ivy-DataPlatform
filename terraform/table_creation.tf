6f0e226eb587f551780911c408babea6b986d096
Alex Crosslen <alex.crosslen@ivyrehab.com>
2024-06-10 16:27:10 +0000

    Updated table_creation.tf

--- terraform/table_creation.tf
+++ terraform/table_creation.tf
@@ -1,14 +1,14 @@
-resource "snowflake_table" "terraform_testing" {
-  database            = var.landing
-  schema              = "RAINTREE"
-  name                = "TERRAFORM_TESTING"
-  change_tracking     = true
+// resource "snowflake_table" "terraform_testing" {
+//   database            = var.landing
+//   schema              = "RAINTREE"
+//   name                = "TERRAFORM_TESTING"
+//   change_tracking     = true
 
-  column {
-    name = "Test"
-    type = "VARCHAR"
-  }
-}
+//   column {
+//     name = "Test"
+//     type = "VARCHAR"
+//   }
+// }
 
 resource "snowflake_table" "ingest_ivyfotoexport_stage" {
   database            = var.landing
