c39283e4a28b1fc5ccc7aee0735152927bc8ac77
Alex Crosslen <alex.crosslen@ivyrehab.com>
2024-06-10 16:44:30 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ terraform {
 
 //------------- TERRAFORM ACCOUNT WITH SNOWFLAKE -----------------
 provider "snowflake" {
-  role = "ACCOUNTADMIN"
+  role = "SYSADMIN"
   account = var.account
   user = var.username
   password = var.password
