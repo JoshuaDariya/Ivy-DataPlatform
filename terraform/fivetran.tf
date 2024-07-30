c668ff2904e3319f660fadc2e9bf7f086f35d6dd
Nisaa Pender <nisaa.pender@ivyrehab.com>
2024-07-30 18:49:01 +0000

    Updated fivetran.tf

--- terraform/fivetran.tf
+++ terraform/fivetran.tf
@@ -1,7 +1,7 @@
 resource "snowflake_network_rule" "fivetran_api_network_rule" {
   name       = "fivetran_api_network_rule"
   database   = var.landing
-  schema     = var.raintree_schema
+  schema     = var.raintree_v2_schema
   comment    = "A network rule for Fivetran's API."
   type       = "HOST_PORT"
   mode       = "EGRESS"
