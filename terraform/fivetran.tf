6e27c186fcf4fe0d9c1391374abfd281f7b6bc60
Nisaa Pender <nisaa.pender@ivyrehab.com>
2024-08-09 14:09:50 +0000

    Deleted fivetran.tf

--- terraform/fivetran.tf
+++
@@ -1,9 +0,0 @@
-resource "snowflake_network_rule" "fivetran_api_network_rule" {
-  name       = "fivetran_api_network_rule"
-  database   = var.landing
-  schema     = var.raintree_v2_schema
-  comment    = "A network rule for Fivetran's API."
-  type       = "HOST_PORT"
-  mode       = "EGRESS"
-  value_list = ["api.fivetran.com"]
-}
