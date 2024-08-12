839fe1a6fa4d9b6a25e7887d23cf4ba620d36e60
Ling He <ling.he@ivyrehab.com>
2024-08-12 10:00:50 -0400

    Add snowflakefinancial role to power bi warehouse

--- terraform/warehouse_access.tf
+++ terraform/warehouse_access.tf
@@ -12,7 +12,7 @@ resource "snowflake_warehouse_grant" "fivetran_grant" {
   warehouse_name = "FIVETRAN_WH"
   privilege      = "USAGE"
 
-  roles = [var.loader_role,var.developer_role, var.qa_role , var.prod_role]
+  roles = [var.loader_role, var.developer_role, var.qa_role , var.prod_role]
 
   with_grant_option = false
 }
@@ -21,7 +21,7 @@ resource "snowflake_warehouse_grant" "power_bi_grant" {
   warehouse_name = "POWER_BI_WH"
   privilege      = "USAGE"
 
-  roles = [var.powerbi_role,var.workday_payroll_role]
+  roles = [var.powerbi_role, var.workday_payroll_role]
 
   with_grant_option = false
 }
