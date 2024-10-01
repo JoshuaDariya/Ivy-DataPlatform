023b4de728416b9d033cfe8b90088500096bfdfb
Ling He <ling.he@ivyrehab.com>
2024-10-01 11:09:31 -0400

    Remove task to call fivetran function

--- terraform/workday.tf
+++ terraform/workday.tf
@@ -1,15 +1,4 @@
 
-resource "snowflake_task" "fivetran_run_workday" {
-
-  database  = var.landing
-  schema    = var.workday_workday_schema
-  warehouse = "IVY_WH"
-
-  name          = "FIVETRAN_RUN"
-  sql_statement = "select fivetran_python()"
-
-}
-
 resource "snowflake_task" "run_fivetran_workday_transformation_status" {
 
   comment   = "Task to call CHECK_WORKDAY_TRANSFORMATION_STATUS() procedure to check if workday transformations have finished successfully each day."
