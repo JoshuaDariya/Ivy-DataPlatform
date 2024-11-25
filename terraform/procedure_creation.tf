dec89d142eac0e58e56fceaf09b45cbacd7446dc
Ling He <ling.he@ivyrehab.com>
2024-11-25 12:26:23 -0500

    Updated timezone to eastern time

--- terraform/procedure_creation.tf
+++ terraform/procedure_creation.tf
@@ -2401,7 +2401,8 @@ resource "snowflake_procedure" "check_raintree_acknowledge_file" {
   // Step 2: Check if we have the acknowledge file checking is done:
   var checkLoadingMessageTable = `SELECT 
                                       CASE 
-                                          WHEN DATE(MAX(MESSAGE_DATETIME)) < DATE(CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP)) THEN 'YES'
+                                          CASE 
+                                          WHEN DATE(MAX(MESSAGE_DATETIME)) < DATE(CONVERT_TIMEZONE('America/Los_Angeles', 'America/New_York', CURRENT_TIMESTAMP)) THEN 'YES'
                                           ELSE 'NO'
                                       END AS shouldCheck
                                   FROM raintree_load_tracking`;
@@ -2463,7 +2464,7 @@ resource "snowflake_procedure" "check_raintree_acknowledge_file" {
                   if (successFileFound) {
                   var insertRaintreeLoadTrackingTableQuery = `
                               INSERT INTO raintree_load_tracking(message_datetime, status) 
-                              VALUES ((CAST(CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP) AS TIMESTAMP_NTZ)), 'NEW')`;
+                              VALUES ((CAST(CONVERT_TIMEZONE('America/Los_Angeles', 'America/New_York', CURRENT_TIMESTAMP) AS TIMESTAMP_NTZ)), 'NEW')`;
                       try {
                           snowflake.execute({ sqlText: insertRaintreeLoadTrackingTableQuery });
                       }
